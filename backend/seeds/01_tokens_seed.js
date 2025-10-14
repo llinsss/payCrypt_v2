a5dc8dd14a2af6fe13a95f1d5341d38277f6e36f
abdegenius <johnnieabijah2000@gmail.com>
2025-10-14 10:35:55 +0100

    Update: address creation flow

--- backend/seeds/01_tokens_seed.js
+++ backend/seeds/01_tokens_seed.js
@@ -1,55 +1,59 @@
 export const seed = async (knex) => {
-  await knex("tokens").del();
-  await knex("tokens").insert([
-    {
-      id: 1,
-      address: "0xCa14007Eff0dB1f8135f4C25B34De49AB0d42766",
-      symbol: "STRK",
-      name: "Starknet",
-      decimals: 18,
-      logo_url: "strk.svg",
-      chain: "Starknet (Ethereum L2)",
-      price: 0.143654,
-    },
-    {
-      id: 2,
-      address: "native",
-      symbol: "LSK",
-      name: "Lisk",
-      decimals: 8,
-      logo_url: "lsk.svg",
-      chain: "Lisk",
-      price: 0.42895,
-    },
-    {
-      id: 3,
-      address: "0x4200000000000000000000000000000000000006",
-      symbol: "BASE",
-      name: "Base",
-      decimals: 18,
-      logo_url: "base.svg",
-      chain: "Base (Ethereum L2)",
-      price: 1.0,
-    },
-    {
-      id: 4,
-      address: "native",
-      symbol: "FLOW",
-      name: "Flow",
-      decimals: 8,
-      logo_url: "flow.svg",
-      chain: "Flow",
-      price: 0.45,
-    },
-    {
-      id: 5,
-      address: "0x558e7139800f8bc119f68d23a6126fffd43a66a6",
-      symbol: "U2U",
-      name: "U2U Network",
-      decimals: 18,
-      logo_url: "u2u.png",
-      chain: "U2U Solaris Mainnet",
-      price: 0.0213,
-    },
-  ]);
+  const result = await knex("tokens").count("* as count");
+  const length = Number(result[0].count);
+  if (length < 5) {
+    await knex("tokens").del();
+    await knex("tokens").insert([
+      {
+        id: 1,
+        address: "0xCa14007Eff0dB1f8135f4C25B34De49AB0d42766",
+        symbol: "STRK",
+        name: "Starknet",
+        decimals: 18,
+        logo_url: "strk.svg",
+        chain: "Starknet (Ethereum L2)",
+        price: 0.143654,
+      },
+      {
+        id: 2,
+        address: "native",
+        symbol: "LSK",
+        name: "Lisk",
+        decimals: 8,
+        logo_url: "lsk.svg",
+        chain: "Lisk",
+        price: 0.42895,
+      },
+      {
+        id: 3,
+        address: "0x4200000000000000000000000000000000000006",
+        symbol: "BASE",
+        name: "Base",
+        decimals: 18,
+        logo_url: "base.svg",
+        chain: "Base (Ethereum L2)",
+        price: 1.0,
+      },
+      {
+        id: 4,
+        address: "native",
+        symbol: "FLOW",
+        name: "Flow",
+        decimals: 8,
+        logo_url: "flow.svg",
+        chain: "Flow",
+        price: 0.45,
+      },
+      {
+        id: 5,
+        address: "0x558e7139800f8bc119f68d23a6126fffd43a66a6",
+        symbol: "U2U",
+        name: "U2U Network",
+        decimals: 18,
+        logo_url: "u2u.png",
+        chain: "U2U Solaris Mainnet",
+        price: 0.0213,
+      },
+    ]);
+  }
 };
--- backend/seeds/02_chains_seed.js
+++ backend/seeds/02_chains_seed.js
@@ -1,46 +1,49 @@
 export const seed = async (knex) => {
-  // Deletes ALL existing entries
-  await knex("chains").del();
-  await knex("chains").insert([
-    {
-      id: 1,
-      symbol: "STRK",
-      name: "Starknet",
-      native_currency: { name: "Starknet Token", symbol: "STRK" },
-      rpc_url: "https://starknet-mainnet.g.alchemy.com/public",
-      block_explorer: "https://starkscan.co",
-    },
-    {
-      id: 2,
-      symbol: "LSK",
-      name: "Lisk",
-      native_currency: { name: "Lisk", symbol: "LSK" },
-      rpc_url: "https://rpc.api.lisk.com",
-      block_explorer: "https://blockscout.lisk.com",
-    },
-    {
-      id: 3,
-      symbol: "BASE",
-      name: "Base",
-      native_currency: { name: "Ether", symbol: "ETH" },
-      rpc_url: "https://mainnet.base.org",
-      block_explorer: "https://basescan.org",
-    },
-    {
-      id: 4,
-      symbol: "FLOW",
-      name: "Flow",
-      native_currency: { name: "Flow Token", symbol: "FLOW" },
-      rpc_url: "https://rest-mainnet.onflow.org",
-      block_explorer: "https://flowscan.org",
-    },
-    {
-      id: 5,
-      symbol: "U2U",
-      name: "U2U",
-      native_currency: { name: "U2U Network", symbol: "U2U" },
-      rpc_url: "https://rpc-mainnet.u2u.xyz",
-      block_explorer: "https://u2uscan.xyz",
-    },
-  ]);
+  const result = await knex("tokens").count("* as count");
+  const length = Number(result[0].count);
+  if (length < 5) {
+    await knex("chains").del();
+    await knex("chains").insert([
+      {
+        id: 1,
+        symbol: "STRK",
+        name: "Starknet",
+        native_currency: { name: "Starknet Token", symbol: "STRK" },
+        rpc_url: "https://starknet-mainnet.g.alchemy.com/public",
+        block_explorer: "https://starkscan.co",
+      },
+      {
+        id: 2,
+        symbol: "LSK",
+        name: "Lisk",
+        native_currency: { name: "Lisk", symbol: "LSK" },
+        rpc_url: "https://rpc.api.lisk.com",
+        block_explorer: "https://blockscout.lisk.com",
+      },
+      {
+        id: 3,
+        symbol: "BASE",
+        name: "Base",
+        native_currency: { name: "Ether", symbol: "ETH" },
+        rpc_url: "https://mainnet.base.org",
+        block_explorer: "https://basescan.org",
+      },
+      {
+        id: 4,
+        symbol: "FLOW",
+        name: "Flow",
+        native_currency: { name: "Flow Token", symbol: "FLOW" },
+        rpc_url: "https://rest-mainnet.onflow.org",
+        block_explorer: "https://flowscan.org",
+      },
+      {
+        id: 5,
+        symbol: "U2U",
+        name: "U2U",
+        native_currency: { name: "U2U Network", symbol: "U2U" },
+        rpc_url: "https://rpc-mainnet.u2u.xyz",
+        block_explorer: "https://u2uscan.xyz",
+      },
+    ]);
+  }
 };
