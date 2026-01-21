ab6412cd3ed0741b0c23d912b031d412b40b0530
dotandev <jesuswrites20043@gmail.com>
2026-01-21 17:46:47 +0100

    feat: implement @tag resolution system with postgres support

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  client: "mysql2",
+  client: "pg",
   connection: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
