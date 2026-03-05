0961f08f835093d49a932f34e58a21bbae4fc335
llins <llinsomoudu@gmail.com>
2026-03-05 13:33:10 +0100

    workers

--- backend/workers.js
+++ backend/workers.js
@@ -1,4 +1,10 @@
 import "./workers/balance.js";
 import "./workers/scheduler.js";
 import "./queues/exportQueue.js";
+import UssdService from "./services/UssdService.js";
 // import "./workers/starknet.js";
+
+// Clean up expired USSD sessions every 5 minutes
+setInterval(() => {
+  UssdService.cleanupExpiredSessions();
+}, 5 * 60 * 1000);
