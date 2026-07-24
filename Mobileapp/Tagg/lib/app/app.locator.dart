9607d0cba43f329986e2b9d500ed835e157f832b
madisonsc52-del <madisonsc52@gmail.com>
2026-07-24 09:13:47 +0100

    #377 Add Offline Mode and Connectivity Handling to Mobile App FIXED

--- Mobileapp/Tagg/lib/app/app.locator.dart
+++ Mobileapp/Tagg/lib/app/app.locator.dart
@@ -16,6 +16,7 @@ import '../services/api_service.dart';
 import '../services/auth_service.dart';
 import '../services/chains_service.dart';
 import '../services/transaction_service.dart';
+import '../services/connectivity_service.dart';
 import '../services/user_service.dart';
 import '../services/wallet_service.dart';
 
@@ -40,4 +41,5 @@ Future<void> setupLocator({
   locator.registerLazySingleton(() => WalletService());
   locator.registerLazySingleton(() => TransactionService());
   locator.registerLazySingleton(() => ChainsService());
+  locator.registerLazySingleton(() => ConnectivityService());
 }
